1b524bfa8e08ecb03cb7921f7d3d9690f72c743d
BMorenoC <bra.morenoc@gmail.com>
2026-01-09 12:16:29 -0300

    Primer Commit

--- src/demo.ts
+++ src/demo.ts
@@ -1,35 +1,6 @@
-async () => {
-  const myCart = [];
-  const products: string | any[] = [];
-  const limit = 2;
+type UserId = string | number;
+let UserId: UserId;
+UserId = 'user123';
+UserId = 456;
 
-  async function getProducts() {
-    const rta = await fetch("http://api.escuelajs.co/api/v1/products", {
-      method: "GET",
-    });
-    const data = await rta.json();
-    products.concat(data);
-  }
-  function getTotal() {
-    let total = 0;
-    for (let i = 0; i < products.length; i++) {
-      total += products[i].prize;
-    }
-    return total;
-  }
-  function addProduct(index: number) {
-    if (getTotal() <= limit) {
-      myCart.push(products[index]);
-    }
-  }
-
-  await getProducts();
-  addProduct(1);
-  addProduct(2);
-  const total = getTotal();
-  console.log(total);
-  const person = {
-    name: "Nicolas",
-    lastName: "Molina",
-  };
-};
+console.log('UserId:', UserId);
